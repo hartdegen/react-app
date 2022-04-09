9b8477977542a550a532a38326c2aae4e2aac3e6
hartdegen <hartdegen182@gmail.com>
2022-04-09 22:56:43 +0300

    react collections

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import Card from './components/Card.jsx';
+import DefinitionsList from './components/DefinitionsList.jsx';
+import Progress from './components/Progress.jsx';
 
 ReactDOM.render(
-  <Card cardTitle="111" text="some text here"/>,
+  <Card cardTitle="111"/>,
   document.getElementById('container1'),
 );
+
+const definitions = [
+  { dt: 'one', dd: 'two', id: Math.random() },
+  { dt: 'another term', dd: 'another description', id: Math.random() },
+];
 ReactDOM.render(
-  <Card cardTitle="222"/>,
+  <DefinitionsList data={definitions}/>,
   document.getElementById('container2'),
+);
+
+ReactDOM.render(
+  <Progress percentage="90"/>,
+  document.getElementById('container3'),
 );
